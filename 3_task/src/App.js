b9bfc7da7abea1f912a035b411edc0d7ae569b25
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-03-30 12:26:11 +0300

    Adding remove and reset button

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -3,19 +3,31 @@ import './App.css';
 
 class App extends Component {
   state = {
-    likes : 0
+    likes: 0
   };
-  addHandler = () =>{
-    console.log('Test')
-    this.setState( {likes: this.state.likes + 1 });
+  addHandler = () => {
+    console.log("Test");
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
+  removeHandler = () => {
+    console.log("Test");
+    this.setState({ likes: this.state.likes - 1 });
+  };
+
+  resetHandler = () => {
+    console.log("Test");
+    this.setState({ likes: this.state.likes = 0 });
   };
 
   render() {
     return (
-    <div>
-      <h1>Total likes:{this.state.likes}</h1>
-      <button onClick={this.addHandler}>Add likes</button>
-    </div>
+      <div>
+        <h1>Total likes:{this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add likes</button>
+        <button onClick={this.removeHandler}>Remove likes</button>
+        <button onClick={this.resetHandler}>Reset likes</button>
+      </div>
     );
   }
 }
