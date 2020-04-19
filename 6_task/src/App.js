94d7ae82345077f1554a6a5ba9da9a333d4f7e5d
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-04-19 13:23:25 +0300

    Post.js and Post.css files created:

--- 6_task/src/App.js
+++ 6_task/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Post from "./Post/Post";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const posts = [
+  {
+    id: 1,
+    title: "Lorem inpsum er ganske enkelt flydtekst",
+    author: "Baboucarr Touray",
+    desc: "Lorem inpsum er ganske enkelt flydtekst",
+    img: "https://source.unsplash.com/featured/laptop",
+  },
+
+  {
+    id: 2,
+    title: "Lorem inpsum er ganske enkelt flydtekst",
+    author: "Mariatou Touray",
+    desc: "Lorem inpsum er ganske enkelt flydtekst",
+    img: "https://source.unsplash.com/photos/AeVemaUw7NM",
+  },
+
+  {
+    id: 3,
+    title: "Lorem inpsum er ganske enkelt flydtekst",
+    author: "Ancha Touray",
+    desc: "Lorem inpsum er ganske enkelt flydtekst",
+    img: "https://unsplash.com/photos/xP_AGmeEa6s",
+  },
+];
+
+class App extends Component {
+  state = {
+    posts: posts,
+  };
+
+  removeHandler = (removeId) => {
+    const oldArray = [...this.state.posts];
+    oldArray.splice(removeId, 1);
+    this.setState({ posts: oldArray });
+  };
+
+  render() {
+    const postList = this.state.posts.map((post, index) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          desc={post.desc}
+          img={post.img}
+          click={this.removeHandler.bind(this, index)}
+        />
+      );
+    });
+
+    return <div className="posts">{postList}</div>;
+  }
 }
-
 export default App;
